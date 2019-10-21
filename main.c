fca3726d52155ad43b56bd49c589609fa26a63e6
Kuryu86 <Kuryu86+github@gmail.com>
2019-10-22 01:21:43 +0900

    抜けていた仕様の追加 リストを表示する関数の改良

--- main.c
+++ main.c
@@ -9,17 +9,36 @@ typedef struct two_way_list
 
 two_way_list *Link_Head(two_way_list *head, two_way_list *target)
 {
+	if (head == NULL)
+	{
+		target->preview = NULL;
+		target->next = NULL;
+		return target;
+	}
 	head->preview = target;
 	target->next = head;
 	target->preview = NULL;
 	return target;
 }
+two_way_list *Link_End(two_way_list *end, two_way_list *target)
+{
+
+	end->next = target;
+	target->next = NULL;
+	target->preview = end;
+	return target;
+}
 void showList(two_way_list *start, two_way_list *end)
 {
+	if (start == NULL)
+	{
+		printf("\n");
+		return;
+	}
 	two_way_list *tmp = start;
 	while (tmp != end->next)
 	{
-		if(tmp!=start)
+		if (tmp != start)
 			printf(",");
 		printf("%d", tmp->date);
 		tmp = tmp->next;
@@ -34,12 +53,13 @@ int main(int argc, char *argv[])
 				 *tmp = NULL;
 	while (fgets(str, sizeof(str), stdin))
 	{
-		if (atoi(str) == 0)
+		int tmpi = atoi(str);
+		if (tmpi == 0)
 		{
 			showList(header, end);
 			continue;
 		}
-		if (atoi(str) == 100)
+		if (tmpi == 100)
 		{
 			tmp = header;
 			header = tmp->next;
@@ -47,7 +67,7 @@ int main(int argc, char *argv[])
 			free(tmp);
 			continue;
 		}
-		if (atoi(str) == -100)
+		if (tmpi == -100)
 		{
 			tmp = end;
 			end = tmp->preview;
@@ -56,12 +76,19 @@ int main(int argc, char *argv[])
 			continue;
 		}
 		tmp = (two_way_list *)malloc(sizeof(two_way_list));
-		tmp->date = atoi(str);
-		if (header == NULL)
+		tmp->date = tmpi;
+		if (header == NULL && end == NULL)
 		{
+			tmp->preview = NULL;
+			tmp->next = NULL;
 			header = tmp;
 			end = tmp;
-			tmp->next = NULL;
+			continue;
+		}
+		if (tmpi < 0)
+		{
+			tmp->date *= -1;
+			end = Link_End(end, tmp);
 		}
 		else
 		{
