24452147849a9d5c94ccc01f592ad2401e67133c
Kuryu86 <Kuryu86+github@gmail.com>
2019-10-21 20:56:13 +0900

    双方向リスト構造体の定義

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+typedef struct 
+{
+	int date;
+	Bidirectional_list *preview;
+	Bidirectional_list *next;
+}Bidirectional_list;
+
 
 int main(int argc, char *argv[]) 
 {
     char str[7];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+	Bidirectional_list *header, *end;
+	while (fgets(str, sizeof(str), stdin))
+	{
+		printf("%s", str);
     }
 
     return 0;
