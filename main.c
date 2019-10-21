0e9a504e8fc8495eb30dcc21045798a506f4085c
Kuryu86 <Kuryu86+github@gmail.com>
2019-10-22 00:46:28 +0900

    リストに追加する関数の追加 リストを表示する関数の追加

--- main.c
+++ main.c
@@ -1,20 +1,55 @@
 #include <stdio.h>
-typedef struct 
+#include <stdlib.h>
+typedef struct two_way_list
 {
 	int date;
-	Bidirectional_list *preview;
-	Bidirectional_list *next;
-}Bidirectional_list;
+	struct two_way_list *preview;
+	struct two_way_list *next;
+} two_way_list;
 
-
-int main(int argc, char *argv[]) 
+two_way_list *Link_Head(two_way_list *head, two_way_list *target)
 {
-    char str[7];
-	Bidirectional_list *header, *end;
+	head->preview = target;
+	target->next = head;
+	target->preview = NULL;
+	return target;
+}
+void showList(two_way_list *start, two_way_list *end)
+{
+	two_way_list *tmp = start;
+	while (tmp != end->next)
+	{
+		printf("%d", tmp->date);
+		tmp = tmp->next;
+	}
+	printf("\n");
+}
+int main(int argc, char *argv[])
+{
+	char str[7];
+	two_way_list *header = NULL,
+				 *end = NULL,
+				 *tmp = NULL;
 	while (fgets(str, sizeof(str), stdin))
 	{
-		printf("%s", str);
-    }
-
-    return 0;
+		if (atoi(str) == 0)
+		{
+			showList(header, end);
+			continue;
+		}
+		
+		tmp = (two_way_list *)malloc(sizeof(two_way_list));
+		tmp->date = atoi(str);
+		if (header == NULL)
+		{
+			header = tmp;
+			end = tmp;
+			tmp->next = NULL;
+		}
+		else
+		{
+			header = Link_Head(header, tmp);
+		}
+	}
+	return 0;
 }
