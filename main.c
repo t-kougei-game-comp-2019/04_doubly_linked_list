2a99f136e476d6f69e433e2fa26ba68119e251c8
yasu2520 <52170130+yasu2520@users.noreply.github.com>
2019-10-27 11:19:14 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,72 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    char str[7];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
+	char str[7];
+	int array[100] = { 0 };
 
-    return 0;
+	while (fgets(str, sizeof(str), stdin)) {
+
+		int input = atoi(str);
+
+		if (input == 100)
+		{
+			printf("%d\n", array[0]);
+			for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+			{
+				if (i + 1 < sizeof(array) / sizeof(array[0]) && array[i] != 0)
+					array[i] = array[i + 1];
+				else
+					break;
+			}
+		}
+		else if (input == -100)
+		{
+
+			for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+			{
+				if (array[sizeof(array) / sizeof(array[0]) - 1 - i] != 0)
+				{ 
+					printf("%d\n", array[sizeof(array) / sizeof(array[0]) - 1 - i]);
+					array[sizeof(array) / sizeof(array[0]) - 1 - i] = 0;
+					break;
+				}					
+			}
+		}
+		else if (input == 0)
+		{
+				for (int i = 0; i < sizeof(array) / sizeof(array[0]) && array[i] != 0; i++)
+				{
+					if (i == 0)
+						printf("%d", array[i]);
+					else
+						printf(",%d", array[i]);
+				}
+			printf("\n");
+		}
+		else
+		{
+			if (input > 0)
+			{
+				for (int i = 0; i < sizeof(array) / sizeof(array[0]) - 1; i++)
+					array[sizeof(array) / sizeof(array[0]) - 1 - i] = array[sizeof(array) / sizeof(array[0]) - 2 - i];
+
+				array[0] = input;
+			}
+			else
+			{
+				for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+				{
+					if (array[i] == 0)
+					{
+						array[i] = -input;
+						break;
+					}
+				}
+			}
+
+		}
+	}
+	return 0;
 }
